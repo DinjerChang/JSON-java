ff247febf495e773e2d54d74b5363ce25e2b758e
eric052199 <eric052199@gmail.com>
2023-02-09 17:28:23 -0800

    test added

--- src/test/java/org/json/junit/XMLTest.java
+++ src/test/java/org/json/junit/XMLTest.java
@@ -1544,6 +1544,52 @@ public class XMLTest {
             assertEquals(e.getMessage(), expected);
         }
     }
+
+    @Test
+    public void testM3WithDuplicateKeyName(){
+        String xmlStr ="<?xml version=\"1.0\"?>\n" +
+                "<catalog>\n" +
+                "    <book id=\"bk101\">\n" +
+                "        <author>Gambardella, Matthew</author>\n" +
+                "        <title>XML Developer's Guide</title>\n" +
+                "        <genre>Computer</genre>\n" +
+                "        <price>44.95</price>\n" +
+                "        <publish_date>2000-10-01</publish_date>\n" +
+                "        <description>An in-depth look at creating applications\n" +
+                "            with XML.</description>\n" +
+                "    </book>\n" +
+                "    <book id=\"bk102\">\n" +
+                "        <author>Ralls, Kim</author>\n" +
+                "        <title>Midnight Rain</title>\n" +
+                "        <genre>Fantasy</genre>\n" +
+                "        <price>5.95</price>\n" +
+                "        <publish_date>2000-12-16</publish_date>\n" +
+                "        <description>A former architect battles corporate zombies,\n" +
+                "            an evil sorceress, and her own childhood to become queen\n" +
+                "            of the world.</description>\n" +
+                "    </book>\n" +
+                "    <book id=\"bk103\">\n" +
+                "        <author>Corets, Eva</author>\n" +
+                "        <title>Maeve Ascendant</title>\n" +
+                "        <genre>Fantasy</genre>\n" +
+                "        <price>5.95</price>\n" +
+                "        <publish_date>2000-11-17</publish_date>\n" +
+                "        <description>After the collapse of a nanotechnology\n" +
+                "            society in England, the young survivors lay the\n" +
+                "            foundation for a new society.</description>\n" +
+                "    </book>\n" +
+                "</catalog>";
+        try {
+            Reader reader = new StringReader(xmlStr);
+            JSONObject jo = XML.toJSONObject(reader, s -> "random string");
+            fail("Should have thrown duplicate exception. ");
+
+        }
+        catch (JSONException e){
+            String expected = "Invalid: transform key to all the same string";
+            assertEquals(e.getMessage(), expected);
+        }
+    }
 }
 
 
